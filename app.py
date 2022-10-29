e1efd2dbbbb7cc5b74008e75fc58e7867f01f4a1
Haendel Moreira <haendelcavalcante@gmail.com>
2022-10-29 13:02:25 -0300

    Correcao no ajuste da quatidade de cores

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 import io
 from pathlib import Path
 import streamlit as st
-from sklearn.cluster import KMeans
+from matplotlib.colors import to_hex
 from PIL import Image
 import numpy as np
-from matplotlib.colors import to_hex
 from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
 
-def get(imagem_carregada):
+
+def get(imagem_carregada, n_cores):
     # salvar a imagem do streamlit
     with open('imagem.jpg', 'wb') as file:
         file.write(imagem_carregada.getbuffer())
     # ler a imagem
     image = Image.open('imagem.jpg')
-    # transformar os pixcels em linhas de uma matriz
-    N,M = image.size
-    X = np.asarray(image).reshape((M*N,3))
+    # transformar os pixels em linhas de uma matriz
+    N, M = image.size
+    X = np.asarray(image).reshape((M*N, 3))
     # criar e aplicar o k-means na imagem
-    model = KMeans(n_clusters=2, random_state=42).fit(X)
-
-    # capturar os centros (cores médias dos grupos )
+    model = KMeans(n_clusters=n_cores, random_state=42).fit(X)
+    # capturar os centros (cores médias dos grupos)
     cores = model.cluster_centers_.astype('uint8')[np.newaxis]
     cores_hex = [to_hex(cor/255) for cor in cores[0]]
 
@@ -37,25 +37,25 @@ def show(cores):
 def save(fig):
     img = io.BytesIO()
     fig.savefig(img, format='png')
-    plt.axis("off")
+    plt.axis('off')
     return img
-    
+
 st.title("Gerador de paletas")
-imagem = st.file_uploader("Envie sua imagem", ["jpg","jpeg"])
+imagem = st.file_uploader("Envie sua imagem", ["jpg", "jpeg"])
 
 col1, col2 = st.columns([.7, .3])
 
 if imagem:
     col1.image(imagem)
-    botao_gerar_paleta = col2.button("Gerar paleta")
     n_cores = col2.slider(
         "Quantidade de cores",
         min_value=2,
         max_value=8,
         value=5
-    )    
+    )
+    botao_gerar_paleta = col2.button("Gerar paleta!")
     if botao_gerar_paleta:
-        cores, cores_hex = get(imagem)
+        cores, cores_hex = get(imagem, n_cores)
         figura = show(cores)
         col2.pyplot(fig=figura)
         col2.code(f"{cores_hex}")
